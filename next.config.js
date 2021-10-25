a7ab944fd11b6679ec9acf81a777ad5d2c8152b5
Mrkou47 <koubodehao@gmail.com>
2021-10-25 13:24:58 +0800

    feat: external react and react-dom

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.externals = {
+        next: 'next',
+        react: 'React',
+        'react-dom': 'ReactDOM'
+      }
+    }
+    return config;
+  }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.0/umd/react.production.min.js" strategy="beforeInteractive" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.0/umd/react-dom.production.min.js" strategy="beforeInteractive" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp
